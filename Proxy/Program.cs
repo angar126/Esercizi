b724532898345134a8848983b9b262a55614f780
angar126 <gargioniandrea@msn.com>
2023-11-29 09:24:29 +0100

    Proxy

---
+++ Proxy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Proxy;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProxySingleton instance = ProxySingleton.GetInstance();
+
+            Console.WriteLine(instance.Ip1);
+            Console.WriteLine(instance.Ip2);
+
+            ProxySingleton instance2 = ProxySingleton.GetInstance();
+
+            Console.WriteLine(instance2.Ip1);
+            Console.WriteLine(instance2.Ip2);
+
+            ProxySingleton instance3 = ProxySingleton.GetInstance();
+
+            Console.WriteLine(instance3.Ip1);
+            Console.WriteLine(instance3.Ip2);
+
+            ProxySingleton instance4 = ProxySingleton.GetInstance();
+
+            Console.WriteLine(instance4.Ip1);
+            Console.WriteLine(instance4.Ip2);
+        }
+    }
+}
---
+++ Proxy/ProxySingleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public class ProxySingleton
+    {
+        private static ProxySingleton _instance;
+        string _ip2;
+        string _ip1;
+        public string Ip1 { get { return _ip1; } }
+        public string Ip2 { get { return _ip2; } }
+        ProxySingleton() {
+            Random rnd = new Random();
+            _ip1 = $"{rnd.Next(0, 256)}.{rnd.Next(0, 256)}.{rnd.Next(0, 256)}.{rnd.Next(0, 256)}";
+            _ip2 = $"{rnd.Next(0, 256)}.{rnd.Next(0, 256)}.{rnd.Next(0, 256)}.{rnd.Next(0, 256)}"; 
+        }
+        public static ProxySingleton GetInstance() {
+            if (_instance == null) {
+                _instance = new ProxySingleton();
+                    }
+            return _instance; 
+        }
+
+    }
+}
