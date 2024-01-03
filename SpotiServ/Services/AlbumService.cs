d3077db5ff0ab3ef1b913d0307c3c78478360356
angar126 <gargioniandrea@msn.com>
2024-01-03 18:05:24 +0100

    prova

--- SpotiServ/Services/AlbumService.cs
+++ SpotiServ/Services/AlbumService.cs
@@ -1,22 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using SpotiData;
+using Newtonsoft.Json;
 
 namespace SpotiServ.Services
 {
     public class AlbumService : IService<Album, AlbumDTO>
     {
-
-        readonly MusicRepository<Album, AlbumDTO, AlbumDTO> _MusicRepository;
+        private readonly string spotiApiBaseUrl = "http://172.18.0.3:8000"; // Sostituisci con l'indirizzo effettivo del tuo servizio SpotiAPI
+        private readonly HttpClient httpClient;
 
         static AlbumService instance;
         AlbumService()
         {
-            _MusicRepository = new MusicRepository<Album, AlbumDTO, AlbumDTO>(@"C:\Users\user\Desktop\csv");
+            httpClient = new HttpClient();
         }
+
         public static AlbumService GetInstance()
         {
             if (instance is null)
@@ -25,20 +28,66 @@ namespace SpotiServ.Services
             }
             return instance;
         }
-        public List<AlbumDTO> GetAll()
+
+        public async Task<List<AlbumDTO>> GetAllApi()
         {
-
-            return _MusicRepository.GetAll().ToList();
-
+            var response = await httpClient.GetStringAsync($"{spotiApiBaseUrl}/api/Album");
+            return JsonConvert.DeserializeObject<List<AlbumDTO>>(response);
         }
+
+        public async Task<AlbumDTO> GetApi(int Id)
+        {
+            var response = await httpClient.GetStringAsync($"{spotiApiBaseUrl}/api/Album/{Id}");
+            return JsonConvert.DeserializeObject<AlbumDTO>(response);
+        }
+        public void Dispose()
+        {
+            httpClient.Dispose();
+        }
+        //li rimando sincroni per prova
         public AlbumDTO Get(int Id)
         {
-            List<AlbumDTO> items = GetAll();
-            return items.FirstOrDefault(s => s.Id == Id);
+            return GetApi(Id).Result;
+        }
+        public List<AlbumDTO> GetAll()
+        {
+            return GetAllApi().Result;
         }
         public AlbumDTO Update() { return null; }
         public AlbumDTO Update(AlbumDTO album) { return null; }
         public AlbumDTO Delete(int Id) { return null; }
     }
-
 }
+    //public class AlbumService : IService<Album, AlbumDTO>
+    //{
+
+//readonly MusicRepository<Album, AlbumDTO, AlbumDTO> _MusicRepository;
+
+//static AlbumService instance;
+//AlbumService()
+//{
+//    _MusicRepository = new MusicRepository<Album, AlbumDTO, AlbumDTO>(@"C:\Users\user\Desktop\csv");
+//}
+//public static AlbumService GetInstance()
+//{
+//    if (instance is null)
+//    {
+//        instance = new AlbumService();
+//    }
+//    return instance;
+//}
+//public List<AlbumDTO> GetAll()
+//{
+
+//    return _MusicRepository.GetAll().ToList();
+
+//}
+//public AlbumDTO Get(int Id)
+//{
+//    List<AlbumDTO> items = GetAll();
+//    return items.FirstOrDefault(s => s.Id == Id);
+//}
+//public AlbumDTO Update() { return null; }
+//public AlbumDTO Update(AlbumDTO album) { return null; }
+//public AlbumDTO Delete(int Id) { return null; }
+//}
--- SpotiServ/Services/ArtistService.cs
+++ SpotiServ/Services/ArtistService.cs
@@ -1,22 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using SpotiData;
 
 namespace SpotiServ.Services
 {
     public class ArtistService : IService<Artist, ArtistDTO>
     {
-
-        readonly MusicRepository<Artist, ArtistDTO, ArtistDTO> _MusicRepository;
+        private readonly string spotiApiBaseUrl = "http://172.18.0.3:8000"; // Sostituisci con l'indirizzo effettivo del tuo servizio SpotiAPI
+        private readonly HttpClient httpClient;
 
         static ArtistService instance;
         ArtistService()
         {
-            _MusicRepository = new MusicRepository<Artist, ArtistDTO, ArtistDTO>(@"C:\Users\user\Desktop\csv");
+            httpClient = new HttpClient();
         }
+
         public static ArtistService GetInstance()
         {
             if (instance is null)
@@ -25,19 +28,62 @@ namespace SpotiServ.Services
             }
             return instance;
         }
-        public List<ArtistDTO> GetAll()
+
+        public async Task<List<ArtistDTO>> GetAllApi()
         {
-
-            return _MusicRepository.GetAll().ToList();//_MusicRepository.GetAll().Select(i => new SongDTO(i)).ToList();
-
+            var response = await httpClient.GetStringAsync($"{spotiApiBaseUrl}/api/Artist");
+            return JsonConvert.DeserializeObject<List<ArtistDTO>>(response);
         }
+
+        public async Task<ArtistDTO> GetApi(int Id)
+        {
+            var response = await httpClient.GetStringAsync($"{spotiApiBaseUrl}/api/Artist/{Id}");
+            return JsonConvert.DeserializeObject<ArtistDTO>(response);
+        }
+        public void Dispose()
+        {
+            httpClient.Dispose();
+        }
+        //li rimando sincroni per prova
         public ArtistDTO Get(int Id)
         {
-            List<ArtistDTO> items = GetAll();
-            return items.FirstOrDefault(s => s.Id == Id);
+            return GetApi(Id).Result;
+        }
+        public List<ArtistDTO> GetAll()
+        {
+            return GetAllApi().Result;
         }
         public ArtistDTO Update() { return null; }
         public ArtistDTO Update(ArtistDTO artist) { return null; }
         public ArtistDTO Delete(int Id) { return null; }
     }
+    //readonly MusicRepository<Artist, ArtistDTO, ArtistDTO> _MusicRepository;
+
+    //static ArtistService instance;
+    //ArtistService()
+    //{
+    //    _MusicRepository = new MusicRepository<Artist, ArtistDTO, ArtistDTO>(@"C:\Users\user\Desktop\csv");
+    //}
+    //public static ArtistService GetInstance()
+    //{
+    //    if (instance is null)
+    //    {
+    //        instance = new ArtistService();
+    //    }
+    //    return instance;
+    //}
+    //public List<ArtistDTO> GetAll()
+    //{
+
+    //    return _MusicRepository.GetAll().ToList();//_MusicRepository.GetAll().Select(i => new SongDTO(i)).ToList();
+
+    //}
+    //public ArtistDTO Get(int Id)
+    //{
+    //    List<ArtistDTO> items = GetAll();
+    //    return items.FirstOrDefault(s => s.Id == Id);
+    //}
+    //public ArtistDTO Update() { return null; }
+    //public ArtistDTO Update(ArtistDTO artist) { return null; }
+    //public ArtistDTO Delete(int Id) { return null; }
 }
